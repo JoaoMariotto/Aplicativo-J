5fa24163ff0bcae7ae7b3b9c5e3b94bc2ace174a
JoaoMariotto <joaomariotto2006@gmail.com>
2024-03-08 14:33:31 -0300

    Html é mais facil

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,19 +1,49 @@
-import { View } from "react-native";
-import Header from "../components/Header";
-import Stories from "../components/Stories"
-
+import { StyleSheet, View, Image, TouchableOpacity, ScrollView} from 'react-native';
+import Header from '../components/Header';
+import Stories from '../components/Stories';
+import Post from '../components/Post';
+import PessoaProfile from "../assets/yag.jpg";
+import postImage from "../assets/post-example.png";
 
 export default function Home() {
   return (
-    <View
-      style={{
-        flex: 1,
-        marginVertical: 32,
-        marginHorizontal: 16,
-      }}
-    >
-     <Header />
+    <ScrollView style={styles.container}>
+      <Header/>
       <Stories/>
-    </View>
+      <View style={styles.borda}></View>
+        <Post style={styles.post}
+        profileName={"Fulano da Silva"}
+        profileImage={PessoaProfile}
+        postImage={postImage}
+        description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla laoreet euismod lorem, vel tristique nisl egestas nec. In rutrum nibh in quam suscipit mollis. In accumsan egestas tellus, id porttitor risus dictum non. "}/>
+      <Post
+        profileName={"Fulano da Silva"}
+        profileImage={PessoaProfile}
+        postImage={postImage}
+        description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla laoreet euismod lorem, vel tristique nisl egestas nec. In rutrum nibh in quam suscipit mollis. In accumsan egestas tellus, id porttitor risus dictum non. "}/>
+      <Post
+        profileName={"Fulano da Silva"}
+        profileImage={PessoaProfile}
+        postImage={postImage}
+        description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla laoreet euismod lorem, vel tristique nisl egestas nec. In rutrum nibh in quam suscipit mollis. In accumsan egestas tellus, id porttitor risus dictum non. "}/>
+      </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    marginTop: 35,
+    padding: 5,
+    borderRadius: 5,
+  },
+  borda: {
+    borderBottomWidth: 1,
+    opacity: .5,
+    marginVertical: 8,
+},
+post: {
+  marginVertical: 10,
+},
+});
